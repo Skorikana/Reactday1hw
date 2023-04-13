dcabc0885cc46f720317d654f3734e64d892ed09
Skorikana <sandy.pramoda90@gmail.com>
2023-04-13 00:09:58 -0400

    commit

--- src/components/Webvisitors.js
+++ src/components/Webvisitors.js
@@ -1,10 +1,12 @@
-//import {useState} from "react";
 const WebVisitors= () => {
+  const myArray = ["120 people visited on Sunday" ,"240 people visited on Monday" ,"360 people visited on Tuesday","100 people visited on Wednesday"];
+  const myList =myArray.map(((item) => <p>{item}</p>))
   return( 
   <div className ="webVisitors">
     <h3>WebsiteVisitors</h3>
-       <h3>821</h3>
-       <div className ="sub">     
+       <h4>Total visitors in this week:821</h4>
+      <div className ="sub">  
+       <p>{myList}</p>   
    </div>
 </div>
 )
